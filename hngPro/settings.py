ddc2ea4a1192fc03c14b8676b748da8d861d41e6
elnino001 <henryenare001@gmail.com>
2022-10-27 15:26:49 +0100

    fouth commit

--- hngPro/settings.py
+++ hngPro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kmb^85on*lu7@v5q5*q&khwzf^urwgk4=13)fg*ym^kn=e46w)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

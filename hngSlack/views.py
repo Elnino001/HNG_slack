1ed7cf7550da2169f08f3d52910e61885f12ec54
elnino001 <henryenare001@gmail.com>
2022-10-28 20:51:15 +0100

    updated

--- hngSlack/views.py
+++ hngSlack/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from rest_framework import generics
-from . import serializers, models
+from rest_framework.response import Response
+from rest_framework.views import APIView 
+from django.http import JsonResponse
+from .models import HngSlackDetails
 
-class HngDetailsList(generics.ListAPIView):
-    serializer_class = serializers.HngSerializer
-    queryset = models.HngSlackDetails.objects.all()
-    
+# class HngDetailsList(generics.ListAPIView):
+#     serializer_class = serializers.HngSerializer
+#     queryset = models.HngSlackDetails.objects.all()
+
+class HngDetailsList(APIView):
+    def get(self, *args, **kwargs):
+        data = {
+            "slackUsername": "Aniche",
+            "backend": True,
+            "age": 28,
+            "bio": "I am a python developer"
+        } 
+
+        return Response(data)
